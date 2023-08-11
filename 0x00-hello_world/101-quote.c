41ae43e51a3166d8ac9b56fc23e9618b44884641
demerwdani7452 <demerwdani@gmail.com>
2023-08-10 19:53:16 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - A C program that prints a line with fputs function
+ 
+* Return: 0 (Success)
+*/
+int main(void)
+{
+fputs("\"and that piece of art is useful\" - Dora Kerpar, 2015.10.19\n", stdout);
+return (1);
+}
