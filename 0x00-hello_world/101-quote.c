0053d81cc6f026a6a74b016074448e5e87952829
demerwdani7452 <demerwdani@gmail.com>
2023-08-12 16:30:03 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
-* main - A C program that prints a line with fputs function
- 
-* Return: 0 (Success)
+* main - Entry point
+*
+* Return: Always 1 (Success)
 */
 int main(void)
 {
-fputs("\"and that piece of art is useful\" - Dora Kerpar, 2015.10.19\n", stdout);
-return (1);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
+
